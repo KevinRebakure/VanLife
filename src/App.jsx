c4d18ff9a08123b9eaabfda123ce10e57e720548
Rebakure <kevinrebakure@gmail.com>
2024-07-31 19:09:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
