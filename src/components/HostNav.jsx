083be2a10d7fd2459dea0b8d9e005c9e6719f6a1
Rebakure <kevinrebakure@gmail.com>
2024-08-01 19:29:53 +0200

    Update HostNav.jsx

--- src/components/HostNav.jsx
+++ src/components/HostNav.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function HostNav() {
   return (
     <>
-      <div className="space-x-3">
+      <div className="gap-x-3 space-x-3">
         <NavLink
           className={({ isActive }) =>
             isActive ? "underline" : "hover:underline"
