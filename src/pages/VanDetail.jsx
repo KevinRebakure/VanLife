6fe4eae741575357b620a9924c1b9f78e0b80455
Rebakure <kevinrebakure@gmail.com>
2024-08-01 11:54:15 +0200

    Update VanDetail.jsx

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -4,16 +4,15 @@ import Nav1 from "../components/Nav1";
 import { useEffect } from "react";
 import { useState } from "react";
 export default function VanDetail() {
-  const [{ description, id, imageUrl, name, price, type }, setVanData] =
-    useState({});
   const params = useParams();
+  const [vanData, setVanData] = useState({});
 
   const buttonColors = {
     simple: "bg-[#E17654]",
     luxury: "bg-[#161616]",
     rugged: "bg-[#115E59]",
   };
-  console.log(params);
+  // console.log(params);
   useEffect(() => {
     fetch(`/api/vans/${params.id}`)
       .then((res) => res.json())
@@ -23,38 +22,43 @@ export default function VanDetail() {
   return (
     <>
       <Nav1 />
-      <div className="space-y-4 p-4">
-        <Link
-          to="/vans"
-          className="font-semibold underline hover:text-gray-400"
-        >
-          Back to all vans
-        </Link>
-        <img
-          src={`${imageUrl}`}
-          alt=""
-          className="h-[500px] w-full rounded-lg object-cover"
-        />
-        <button
-          className={`rounded-md font-semibold text-white ${buttonColors[type]} px-2 py-1`}
-        >
-          {type}
-        </button>
-        <div>
-          <p className="text-2xl font-semibold">{name}</p>
-          <p>
-            <span className="text-2xl font-semibold">${price}</span>{" "}
-            <span>/day</span>
-          </p>
+
+      {vanData ? (
+        <div className="space-y-4 p-4">
+          <Link
+            to="/vans"
+            className="font-semibold underline hover:text-gray-400"
+          >
+            Back to all vans
+          </Link>
+          <img
+            src={`${vanData.imageUrl}`}
+            alt=""
+            className="h-[500px] w-full rounded-lg object-cover"
+          />
+          <button
+            className={`rounded-md font-semibold text-white ${buttonColors[vanData.type]} px-2 py-1`}
+          >
+            {vanData.type}
+          </button>
+          <div>
+            <p className="text-2xl font-semibold">{vanData.name}</p>
+            <p>
+              <span className="text-2xl font-semibold">${vanData.price}</span>{" "}
+              <span>/day</span>
+            </p>
+          </div>
+          <p>{vanData.description}</p>
+          <button
+            to="/vans"
+            className="block w-full rounded-md bg-[#FF8C38] py-3 text-center font-semibold text-white"
+          >
+            Rent this van
+          </button>
         </div>
-        <p>{description}</p>
-        <button
-          to="/vans"
-          className="block w-full rounded-md bg-[#FF8C38] py-3 text-center font-semibold text-white"
-        >
-          Rent this van
-        </button>
-      </div>
+      ) : (
+        <p>Loading...</p>
+      )}
 
       <Footer />
     </>
