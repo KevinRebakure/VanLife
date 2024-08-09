ee49cb77fab8abfdc621d7d928b023b70734d257
KevinRebakure <kevinrebakure@gmail.com>
2024-08-09 05:26:17 +0200

    Update Vans.jsx

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -21,21 +21,32 @@ export default function Vans() {
   });
 
   // console.log(vans);
+
+  function handleFilterChange(key, value) {
+    setSearchParams((prevParams) => {
+      if (value === null) {
+        prevParams.delete(key);
+      } else {
+        prevParams.set(key, value);
+      }
+      return prevParams;
+    });
+  }
   return (
     <>
       <div className="space-y-3 py-3">
         <h1 className="px-3 text-2xl font-semibold">Explore our vans</h1>
         <div className="flex items-center gap-x-3 px-3">
-          <button onClick={()=>setSearchParams({type: 'simple'})} className="rounded-md bg-[#FFEAD0] px-2 py-1 text-center font-semibold">
+          <button onClick={()=>handleFilterChange('type', 'simple')} className="rounded-md bg-[#FFEAD0] px-2 py-1 text-center font-semibold">
             Simple
           </button>
-          <button onClick={()=>setSearchParams({type: 'luxury'})} className="rounded-md bg-[#FFEAD0] px-2 py-1 text-center font-semibold">
+          <button onClick={()=>handleFilterChange('type', 'luxury')} className="rounded-md bg-[#FFEAD0] px-2 py-1 text-center font-semibold">
             Luxury
           </button>
-          <button onClick={()=>setSearchParams({type: 'rugged'})} className="rounded-md bg-[#FFEAD0] px-2 py-1 text-center font-semibold">
+          <button onClick={()=>handleFilterChange('type', 'rugged')} className="rounded-md bg-[#FFEAD0] px-2 py-1 text-center font-semibold">
             Rugged
           </button>
-          <button onClick={()=>setSearchParams({})} className="ml-auto px-2 py-1 text-center font-semibold underline">
+          <button onClick={()=>handleFilterChange('type', null)} className="ml-auto px-2 py-1 text-center font-semibold underline">
             Clear filters
           </button>
         </div>
